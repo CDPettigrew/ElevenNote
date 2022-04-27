6a7560481420ca96139c2aff8c71d0a3fc7bfddd
chris pettigrew <cdpettigrew26@comcast.net>
2022-04-27 11:45:50 -0400

    added mvc assembly and create method+view

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        private NoteService CreateNotService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
+        }
+        // GET: Note
+        public ActionResult Index()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            var model = service.GetNotes();
+            return View(model);
+        }
+        //GET: Note/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //POST: Note/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var service = CreateNotService();
+            if (service.CreateNote(model))
+            {
+                TempData["SaveResult"] = "Your Note was created.";
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Note could not be created.");
+            return View(model);
+
+        }
+
+    }
+}
