fdf49cb72e088352897fd348a8e04d014d68c659
chris pettigrew <cdpettigrew26@comcast.net>
2022-04-27 12:21:26 -0400

    finished all mvc methods and views

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -12,6 +12,69 @@ namespace ElevenNote.WebMVC.Controllers
     [Authorize]
     public class NoteController : Controller
     {
+        //DETAILS
+        public ActionResult Details(int id)
+        {
+            var svc = CreateNotService();
+            var model = svc.GetNoteById(id);
+            return View(model);
+        }
+        //GET: Note/Edit
+        public ActionResult Edit(int id)
+        {
+            var service = CreateNotService();
+            var detail = service.GetNoteById(id);
+            var model = new NoteEdit
+            {
+                NoteId = detail.NoteID,
+                Title = detail.Title,
+                Content = detail.Content
+            };
+            return View(model);
+        }
+        //POST: Note/Edit
+        [HttpPost]
+        [ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, NoteEdit model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            if(model.NoteId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatched");
+                return View(model);
+            }
+            var service = CreateNotService();
+            if (service.UpdateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was updated.";
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Your note could not be updated.");
+            return View(model);
+        }
+        //GET: Note/Delete
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var svc = CreateNotService();
+            var model = svc.GetNoteById(id);
+            return View(model);
+        }
+        //POST: Note/Delete
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id)
+        {
+            var service = CreateNotService();
+            service.DeleteNote(id);
+            TempData["SaveResult"] = "Your note was deleted.";
+            return RedirectToAction("Index");
+        }
         private NoteService CreateNotService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
